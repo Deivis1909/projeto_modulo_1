8cd21d7ecf29c8b9423db7e30fdb20b0ad314c6a
Deivis1909 <113397116+Deivis1909@users.noreply.github.com>
2023-03-20 11:27:56 -0300

    version_2.0

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { useState,useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
+import ListaProdutos from './components/ListaProdutos';
 //import ListaProdutos from './components/ListaProdutos';
 
 
@@ -12,21 +13,18 @@ function App() {
 
   useEffect(() => {
 
-   fetch('produtos.json')
+  fetch('produtos.json')
    .then(resp => resp.json())
-
-   // pra aonde vai jogar a response do json
 
    //pra onde vai jogar dados do json
    //.then(data =>{
   // })
-
   .then(produtos => { 
     setProdutos(produtos);
     
   } )
   .catch(erro => console.log(erro));
-}, []);
+  }, []);
 
   
 
@@ -34,31 +32,8 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <>
-      <div className='tabela-titulo'>
+      <ListaProdutos produtos = {produtos} />
       
-      </div>
-      <table className='tabela'>
-        <thead>
-          <tr>
-            <th>produto</th>
-            <th>codigo</th>
-            
-          </tr>
-        </thead>
-        <tbody>
-          {
-            produtos.map(prod => 
-              <tr key={prod.codigo}>
-                <td>{prod.descricao}</td>
-                <td>{prod.codigo}</td>
-                
-              </tr>
-            )
-          }
-        </tbody>
-      </table>
-    </>
       
       
     </div>
--- src/components/ListaProdutos/index.js
+++ src/components/ListaProdutos/index.js
@@ -1,8 +1,49 @@
-export default  function ListaProdutos(produtos){
-   //const [lista,setLista] = useState(produtos)
+
+export default  function ListaProdutos({produtos}){
+   
+
+   //const[produtos,setProdutos] = useState([]);
+
+    //useEffect(() => {
+
+    //fetch('produtos.json')
+    //.then(resp => resp.json())
+     //.then(produtos => { 
+    //setProdutos(produtos);
+    
+  //} )
+  //.catch(erro => console.log(erro));
+//}, []);
+
 
     return(
         <div>
+            <>
+      <div className='tabela-titulo'>
+      
+      </div>
+      <table className='tabela'>
+        <thead>
+          <tr>
+            <th>produto</th>
+            <th>codigo</th>
+            
+          </tr>
+        </thead>
+        <tbody>
+          {
+            produtos.map(prod => 
+              <tr key={prod.codigo}>
+                <td>{prod.descricao}</td>
+                <td>{prod.codigo}</td>
+                
+              </tr>
+            )
+          }
+        </tbody>
+      </table>
+    </>
+
          </div>
     )
 }
